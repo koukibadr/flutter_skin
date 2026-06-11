a820ccf64b33f51044de5c88ebbe76115164fc9c
koukibadr <badreddinekouki1998@gmail.com>
2026-06-11 13:26:47 +0300

    feat: update skin schema to follow the database schema

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,8 +5,8 @@ import 'pages/home_page.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await FlutterSkin.init(
-    developerId: '4cd76c05-0e47-49cc-a514-33eb1049b674',
-    projectId: '2735b07a-fea8-451a-b61d-96ec72d29be8',
+    developerId: '327da410-9f8b-4160-bebb-6dde31f26cef',
+    projectId: '8b23ae3c-4ff7-481e-bafe-b503887c38c9',
   );
   runApp(const MyApp());
 }
