2a9d7fd5e36e6b44d02cf0db182024045dafbab3
koukibadr <badreddinekouki1998@gmail.com>
2026-06-18 14:40:19 +0300

    data: update service api call with guarded client

--- lib/services/skin_service.dart
+++ lib/services/skin_service.dart
@@ -13,19 +13,27 @@ class SkinService {
   }
 
   Future<ProjectConfig?> fetchData(String apiKey) async {
+    var client = http.Client();
     try {
-      var client = http.Client();
-      var response = await client.post(
-        Uri.https('fskin-backend.vercel.app', 'fskin/skin'),
-        headers: {'Content-Type': 'application/json'},
-        body: jsonEncode({'apiKey': apiKey}),
-      );
+      var response = await client
+          .post(
+            Uri.https('fskin-backend.vercel.app', 'fskin/skin'),
+            headers: {'Content-Type': 'application/json'},
+            body: jsonEncode({'apiKey': apiKey}),
+          )
+          .timeout(const Duration(seconds: 5));
+
+      if (response.statusCode != 200) {
+        return null;
+      }
+
       var decodedResponse =
           jsonDecode(utf8.decode(response.bodyBytes)) as Map<String, dynamic>;
-      var projectConfig = ProjectConfig.fromMap(decodedResponse);
-      return projectConfig;
+      return ProjectConfig.fromMap(decodedResponse);
     } catch (e) {
       return null;
+    } finally {
+      client.close();
     }
   }
 }
